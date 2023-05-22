8b2050f925f47938c78f296fcb0850c0c5b8e4d2
alphak3y <alphak3y@protonmail.com>
2023-05-22 11:56:45 -0600

    add pool libraries

---
+++ contracts/libraries/pool/QuoteLib.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import '../../interfaces/ICoverPoolStructs.sol';
+import '../Ticks.sol';
+
+
+library QuoteLib {
+
+    function perform(
+        ICoverPoolStructs.QuoteParams memory params,
+        ICoverPoolStructs.SwapCache memory cache
+    ) external view returns (
+        ICoverPoolStructs.SwapCache memory
+    ) {
+    {
+        ICoverPoolStructs.PoolState memory pool = params.zeroForOne ? cache.pool1 : cache.pool0;
+        cache = ICoverPoolStructs.SwapCache({
+            state: cache.state,
+            syncFees: cache.syncFees,
+            constants: cache.constants,
+            pool0: cache.pool0,
+            pool1: cache.pool1,
+            price: pool.price,
+            liquidity: pool.liquidity,
+            amountIn: params.amountIn,
+            auctionDepth: block.timestamp - cache.constants.genesisTime - cache.state.auctionStart,
+            auctionBoost: 0,
+            input: params.amountIn,
+            output: 0,
+            inputBoosted: 0,
+            amountInDelta: 0
+        });
+    }
+        cache = Ticks.quote(params.zeroForOne, params.priceLimit, cache.state, cache, cache.constants);
+        return cache;
+    }
+}
