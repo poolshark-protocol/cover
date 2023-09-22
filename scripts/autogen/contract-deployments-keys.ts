d12e8cb0d5ee1fe7d011930ad42fe8a71aa7525d
alphak3y <alphak3y@protonmail.com>
2023-09-22 13:18:26 -0600

    09-22 deployment

--- scripts/autogen/contract-deployments-keys.ts
+++ scripts/autogen/contract-deployments-keys.ts
@@ -5,4 +5,88 @@ export const CONTRACT_DEPLOYMENT_KEYS: ContractDeploymentsKey[] = [
         networkName: 'arb_goerli',
         objectName: 'positionsLib'
     }
+    {
+        networkName: 'arb_goerli',
+        objectName: 'uniswapV3FactoryMock'
+    },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'uniswapV3PoolMock'
+    },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'uniswapV3Source'
+    },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'tickMapLib'
+    },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'epochMapLib'
+    },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'deltasLib'
+    },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'epochsLib'
+    },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'ticksLib'
+    },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'claimsLib'
+    },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'positionsLib'
+    },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'coverPoolManager'
+    },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'coverPoolFactory'
+    },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'mintCall'
+    },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'burnCall'
+    },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'swapCall'
+    },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'quoteCall'
+    },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'coverPoolImpl'
+    },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'positionERC1155'
+    },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'poolRouter'
+    },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'coverPool'
+    },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'coverPool2'
+    },
 ];
